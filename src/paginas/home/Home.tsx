47b688f0963ec66a2e93398f8487f52e44613aea
Sara Albuquerque <saraalbuquerque@live.com>
2024-02-14 12:43:17 -0300

    first commit

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import './Home.css'
+
+function Home() {
+    return(
+        <>
+            <h1 className="titulo">Manipulando o mundo</h1>
+            <img className="img" src='https://imgb.ifunny.co/images/d78061113b8eab88e7573214934a115b6adbdc0725c294b7dd2d65a3b27259ff_1.jpg' alt="Imagem" />
+        </>
+    )
+
+}
+
+export default Home;
